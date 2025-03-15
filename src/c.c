843952808e5c39c20fe51b7bc5c581fc15dd49a5
Haoxiang Fei <feihaoxiang@idea.edu.cn>
2025-03-15 22:59:15 +0800

    refactor!: use memory.mbt approach for load/store

--- src/c.c
+++ src/c.c
@@ -1,6 +1,5 @@
 #include <moonbit.h>
 #include <stdbool.h>
-#include <stdint.h>
 #include <stdlib.h>
 
 void *
@@ -18,47 +17,12 @@ moonbit_c_is_null(void *ptr) {
   return ptr == NULL;
 }
 
-char
-moonbit_c_load_char(char *ptr) {
-  return *ptr;
-}
-int
-moonbit_c_load_int(int *ptr) {
-  return *ptr;
-}
-float
-moonbit_c_load_float(float *ptr) {
-  return *ptr;
-}
-double
-moonbit_c_load_double(double *ptr) {
-  return *ptr;
-}
-void
-moonbit_c_store_char(char *ptr, char val) {
-  *ptr = val;
-}
-void
-moonbit_c_store_int(int *ptr, int val) {
-  *ptr = val;
-}
-void
-moonbit_c_store_float(float *ptr, float val) {
-  *ptr = val;
-}
-void
-moonbit_c_store_double(double *ptr, double val) {
-  *ptr = val;
-}
-uint64_t
-moonbit_c_ptr_to_uint64(void *ptr) {
-  return (uint64_t)ptr;
-}
-void *
-moonbit_c_ptr_of_uint64(uint64_t ptr) {
-  return (void *)ptr;
-}
 bool
 moonbit_c_pointer_equal(void *ptr1, void *ptr2) {
   return ptr1 == ptr2;
+}
+
+void *
+moonbit_c_malloc(uint64_t size) {
+  return malloc(size);
 }
