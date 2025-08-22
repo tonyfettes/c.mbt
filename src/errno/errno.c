8718764356f462dccc9c96ad8127e2336d2f9eb2
Tony Fettes <tonyfettes@tonyfettes.com>
2025-08-23 00:01:55 +0800

    feat: @errno

---
+++ src/errno/errno.c
@@ -0,0 +1,16 @@
+#include <errno.h>
+#include <moonbit.h>
+#include <stdint.h>
+
+MOONBIT_FFI_EXPORT
+int32_t
+moonbit_c_errno_get() {
+  return errno;
+}
+
+MOONBIT_FFI_EXPORT
+int32_t
+moonbit_c_errno_set(int32_t err) {
+  errno = err;
+  return errno;
+}
--- src/stdio/stdio.c
+++ src/stdio/stdio.c
@@ -1,9 +1,28 @@
 #include "moonbit.h"
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 
 MOONBIT_FFI_EXPORT
+int32_t
+moonbit_c_EOF() {
+  return EOF;
+}
+
+MOONBIT_FFI_EXPORT
 FILE *
-moonbit_c_stdio_stdin() {
+moonbit_c_stdin() {
   return stdin;
+}
+
+MOONBIT_FFI_EXPORT
+FILE *
+moonbit_c_stdout() {
+  return stdout;
+}
+
+MOONBIT_FFI_EXPORT
+FILE *
+moonbit_c_stderr() {
+  return stderr;
 }
