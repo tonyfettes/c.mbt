ffef7192bbc2f14d9eefc483335c93400f3ead5d
Tony Fettes <tonyfettes@tonyfettes.com>
2025-08-23 00:07:38 +0800

    fix: use stub for fgets/fputs

--- src/stdio/stdio.c
+++ src/stdio/stdio.c
@@ -26,3 +26,15 @@ FILE *
 moonbit_c_stderr() {
   return stderr;
 }
+
+MOONBIT_FFI_EXPORT
+bool
+moonbit_c_fgets(moonbit_bytes_t data, FILE *stream) {
+  return fgets((char *)data, Moonbit_array_length(data), stream) != NULL;
+}
+
+MOONBIT_FFI_EXPORT
+int32_t
+moonbit_c_fputs(moonbit_bytes_t data, FILE *stream) {
+  return fputs((const char *)data, stream);
+}
