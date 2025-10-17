e64fb3dc80940961d1e03c642beafa96b53ae27b
Tony Fettes <tonyfettes@tonyfettes.com>
2025-10-17 23:54:13 +0800

    refactor: Pointer

--- src/c.c
+++ src/c.c
@@ -32,6 +32,30 @@ moonbit_c_pointer_equal(void *ptr1, void *ptr2) {
 }
 
 MOONBIT_FFI_EXPORT
+int64_t
+moonbit_c_pointer_compare(uint8_t *ptr1, uint8_t *ptr2) {
+  return (int64_t)(ptr1 - ptr2);
+}
+
+MOONBIT_FFI_EXPORT
+uint8_t *
+moonbit_c_pointer_add(uint8_t *pointer, uint64_t offset) {
+  return pointer + offset;
+}
+
+MOONBIT_FFI_EXPORT
+uint64_t
+moonbit_c_pointer_sub(uint8_t *upper, uint8_t *lower) {
+  return (uint64_t)(upper - lower);
+}
+
+MOONBIT_FFI_EXPORT
+uint64_t
+moonbit_c_sizeof_pointer() {
+  return sizeof(void *);
+}
+
+MOONBIT_FFI_EXPORT
 void *
 moonbit_c_identity(void *pointer) {
   return pointer;
@@ -39,108 +63,108 @@ moonbit_c_identity(void *pointer) {
 
 MOONBIT_FFI_EXPORT
 char
-moonbit_c_load_byte(void *pointer, int32_t index) {
-  return ((char *)pointer)[index];
+moonbit_c_load_byte(void *pointer) {
+  return *((char *)pointer);
 }
 
 MOONBIT_FFI_EXPORT
 int16_t
-moonbit_c_load_int16(void *pointer, int32_t index) {
-  return ((int16_t *)pointer)[index];
+moonbit_c_load_int16(void *pointer) {
+  return *((int16_t *)pointer);
 }
 
 MOONBIT_FFI_EXPORT
 uint16_t
-moonbit_c_load_uint16(void *pointer, int32_t index) {
-  return ((uint16_t *)pointer)[index];
+moonbit_c_load_uint16(void *pointer) {
+  return *((uint16_t *)pointer);
 }
 
 MOONBIT_FFI_EXPORT
 int32_t
-moonbit_c_load_int(void *pointer, int32_t index) {
-  return ((int32_t *)pointer)[index];
+moonbit_c_load_int(void *pointer) {
+  return *((int32_t *)pointer);
 }
 
 MOONBIT_FFI_EXPORT
 uint32_t
-moonbit_c_load_uint(void *pointer, int32_t index) {
-  return ((uint32_t *)pointer)[index];
+moonbit_c_load_uint(void *pointer) {
+  return *((uint32_t *)pointer);
 }
 
 MOONBIT_FFI_EXPORT
 int64_t
-moonbit_c_load_int64(void *pointer, int32_t index) {
-  return ((int64_t *)pointer)[index];
+moonbit_c_load_int64(void *pointer) {
+  return *((int64_t *)pointer);
 }
 
 MOONBIT_FFI_EXPORT
 uint64_t
-moonbit_c_load_uint64(void *pointer, int32_t index) {
-  return ((uint64_t *)pointer)[index];
+moonbit_c_load_uint64(void *pointer) {
+  return *((uint64_t *)pointer);
 }
 
 MOONBIT_FFI_EXPORT
 float
-moonbit_c_load_float(void *pointer, int32_t index) {
-  return ((float *)pointer)[index];
+moonbit_c_load_float(void *pointer) {
+  return *((float *)pointer);
 }
 
 MOONBIT_FFI_EXPORT
 double
-moonbit_c_load_double(void *pointer, int32_t index) {
-  return ((double *)pointer)[index];
+moonbit_c_load_double(void *pointer) {
+  return *((double *)pointer);
 }
 
 MOONBIT_FFI_EXPORT
 void
-moonbit_c_store_byte(void *pointer, int32_t index, char value) {
-  ((char *)pointer)[index] = value;
+moonbit_c_store_byte(void *pointer, char value) {
+  *((char *)pointer) = value;
 }
 
 MOONBIT_FFI_EXPORT
 void
-moonbit_c_store_int16(void *pointer, int32_t index, int16_t value) {
-  ((int16_t *)pointer)[index] = value;
+moonbit_c_store_int16(void *pointer, int16_t value) {
+  *((int16_t *)pointer) = value;
 }
 
 MOONBIT_FFI_EXPORT
 void
-moonbit_c_store_uint16(void *pointer, int32_t index, uint16_t value) {
-  ((uint16_t *)pointer)[index] = value;
+moonbit_c_store_uint16(void *pointer, uint16_t value) {
+  *((uint16_t *)pointer) = value;
 }
 
 MOONBIT_FFI_EXPORT
 void
-moonbit_c_store_int(void *pointer, int32_t index, int32_t value) {
-  ((int32_t *)pointer)[index] = value;
+moonbit_c_store_int(void *pointer, int32_t value) {
+  *((int32_t *)pointer) = value;
 }
 
 MOONBIT_FFI_EXPORT
 void
-moonbit_c_store_uint(void *pointer, int32_t index, uint32_t value) {
-  ((uint32_t *)pointer)[index] = value;
+moonbit_c_store_uint(void *pointer, uint32_t value) {
+  *((uint32_t *)pointer) = value;
 }
 
 MOONBIT_FFI_EXPORT
 void
-moonbit_c_store_int64(void *pointer, int32_t index, int64_t value) {
-  ((int64_t *)pointer)[index] = value;
+moonbit_c_store_int64(void *pointer, int64_t value) {
+  *((int64_t *)pointer) = value;
 }
 
 MOONBIT_FFI_EXPORT
 void
-moonbit_c_store_uint64(void *pointer, int32_t index, uint64_t value) {
-  ((uint64_t *)pointer)[index] = value;
+moonbit_c_store_uint64(void *pointer, uint64_t value) {
+  *((uint64_t *)pointer) = value;
 }
 
 MOONBIT_FFI_EXPORT
 void
-moonbit_c_store_float(void *pointer, int32_t index, float value) {
-  ((float *)pointer)[index] = value;
+moonbit_c_store_float(void *pointer, float value) {
+  *((float *)pointer) = value;
 }
 
 MOONBIT_FFI_EXPORT
 void
-moonbit_c_store_double(void *pointer, int32_t index, double value) {
-  ((double *)pointer)[index] = value;
+moonbit_c_store_double(void *pointer, double value) {
+  *((double *)pointer) = value;
 }
