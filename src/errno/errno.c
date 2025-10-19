cc5f35e8ae72949981b26f3e8fade525ab68e5d0
Haoxiang Fei <feihaoxiang@idea.edu.cn>
2025-10-19 15:30:59 +0800

    refactor: errno, add fputs tests

--- src/errno/errno.c
+++ src/errno/errno.c
@@ -4,13 +4,25 @@
 
 MOONBIT_FFI_EXPORT
 int32_t
-moonbit_c_errno_get() {
+moonbit_tonyfettes_c_errno_get() {
   return errno;
 }
 
 MOONBIT_FFI_EXPORT
 int32_t
-moonbit_c_errno_set(int32_t value) {
+moonbit_tonyfettes_c_errno_set(int32_t value) {
   errno = value;
   return errno;
+}
+
+MOONBIT_FFI_EXPORT
+int32_t
+moonbit_tonyfettes_c_errno_edom() {
+  return EDOM;
+}
+
+MOONBIT_FFI_EXPORT
+int32_t
+moonbit_tonyfettes_c_errno_erange() {
+  return ERANGE;
 }
--- src/signal/signal.c
+++ src/signal/signal.c
@@ -2,46 +2,55 @@
 #include <signal.h>
 
 MOONBIT_FFI_EXPORT
-void (*moonbit_c_SIG_DFL(void))(int) { return SIG_DFL; }
+int32_t
+moonbit_tonyfettes_c_signal_handler_equal(
+  void (*handler1)(int),
+  void (*handler2)(int)
+) {
+  return handler1 == handler2;
+}
 
 MOONBIT_FFI_EXPORT
-void (*moonbit_c_SIG_IGN(void))(int) { return SIG_IGN; }
+void (*moonbit_tonyfettes_c_signal_SIG_DFL(void))(int) { return SIG_DFL; }
 
 MOONBIT_FFI_EXPORT
-void (*moonbit_c_SIG_ERR(void))(int) { return SIG_ERR; }
+void (*moonbit_tonyfettes_c_signal_SIG_IGN(void))(int) { return SIG_IGN; }
+
+MOONBIT_FFI_EXPORT
+void (*moonbit_tonyfettes_c_signal_SIG_ERR(void))(int) { return SIG_ERR; }
 
 MOONBIT_FFI_EXPORT
 int32_t
-moonbit_c_SIGABRT(void) {
+moonbit_tonyfettes_c_signal_SIGABRT(void) {
   return SIGABRT;
 }
 
 MOONBIT_FFI_EXPORT
 int32_t
-moonbit_c_SIGFPE(void) {
+moonbit_tonyfettes_c_signal_SIGFPE(void) {
   return SIGFPE;
 }
 
 MOONBIT_FFI_EXPORT
 int32_t
-moonbit_c_SIGILL(void) {
+moonbit_tonyfettes_c_signal_SIGILL(void) {
   return SIGILL;
 }
 
 MOONBIT_FFI_EXPORT
 int32_t
-moonbit_c_SIGINT(void) {
+moonbit_tonyfettes_c_signal_SIGINT(void) {
   return SIGINT;
 }
 
 MOONBIT_FFI_EXPORT
 int32_t
-moonbit_c_SIGSEGV(void) {
+moonbit_tonyfettes_c_signal_SIGSEGV(void) {
   return SIGSEGV;
 }
 
 MOONBIT_FFI_EXPORT
 int32_t
-moonbit_c_SIGTERM(void) {
+moonbit_tonyfettes_c_signal_SIGTERM(void) {
   return SIGTERM;
 }
